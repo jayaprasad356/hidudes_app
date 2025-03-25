bce65bdf16fb6a015b771080d5af61997c60dbf3
SudharsanKamaraj <sudharsankamaraj3389@gmail.com>
2025-03-25 12:49:33 +0530

    version_13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.gmwapp.hi_dude"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "12.0"
+        versionCode = 13
+        versionName = "13.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
